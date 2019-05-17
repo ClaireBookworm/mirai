3ab03538a24e36bd13638b7e64238d2f1887f095
echogecho <jpbuban@yahoo.com>
2019-05-16 19:51:01 -0500

    Update run_inference.py

--- source/run_inference.py
+++ source/run_inference.py
@@ -281,7 +281,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             verts = np.fliplr(verts) - 1
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
-    #ax.imshow(masked_image.astype(np.uint8))
+    ax.imshow(masked_image.astype(np.uint8))
     plt.savefig('/output/output.jpg')
     
 if __name__ == "__main__":
